09687b0c491c1e1b46973e8ba4d6363531a7236b
eemash <e-mash@kellogg.northwestern.edu>
2015-04-12 16:42:12 -0500

    4.12

--- address_to_weather.rb
+++ address_to_weather.rb
@@ -14,9 +14,13 @@ street_address = gets.chomp
 url_safe_street_address = URI.encode(street_address)
 
 # Your code goes below.
+url_data_we_want= "https://api.forecast.io/forecast/39ab5cf3f65c8a1ad56b02964b2713b9/41.78,-87.59"
+raw_data= open(url_of_data_we_want).read
+parsed_data= JSON.parse(raw_data)
+
+the_latitude=parsed_data["latitude"]
+the_longitude=parsed_data["longitude"]
 
 # Ultimately, we want the following line to work when uncommented:
 
-# puts "The current temperature at #{street_address} is #{the_temperature} degrees."
-# puts "The outlook for the next hour is: #{the_hour_outlook}"
-# puts "The outlook for the next day is: #{the_day_outlook}"
+puts "The latitude of #{street_address} is #{the_latitude} and the longitude is #{the_longitude}."
--- coords_to_weather.rb
+++ coords_to_weather.rb
@@ -15,12 +15,20 @@ the_latitude = gets.chomp
 puts "What is the longitude?"
 the_longitude = gets.chomp
 
+url_data_we_want="https://api.forecast.io/forecast/39ab5cf3f65c8a1ad56b02964b2713b9/41.7890569,-87.5955604"
+raw_data = open(url_of_data_we_want).read
+parsed_data = JSON.parse(raw_data)
+
+the_temperature=parsed_data["results"][0]["currently"]["temperature"]
+the_hour_outlook=parsed_data["results"][0]["hourly"]["summary"]
+the_day_outlook=parsed_data["results"][0]["daily"]["summary"]
+
 # Your code goes below. Use the same approach as you did in
 #   address_to_coords.rb to read from a remote API and parse
 #   the results.
 
 # Ultimately, we want the following line to work when uncommented:
 
-# puts "The current temperature at #{the_latitude}, #{the_longitude} is #{the_temperature} degrees."
-# puts "The outlook for the next hour is: #{the_hour_outlook}"
-# puts "The outlook for the next day is: #{the_day_outlook}"
+puts "The current temperature at #{the_latitude}, #{the_longitude} is #{the_temperature} degrees."
+puts "The outlook for the next hour is: #{the_hour_outlook}"
+ puts "The outlook for the next day is: #{the_day_outlook}"
